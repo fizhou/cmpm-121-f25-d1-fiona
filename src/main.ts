e15475dcb5b086e0bef0d99b612423e211894da5
Fiona Zhou <fizhou@ucsc.edu>
2025-10-08 20:08:14 +0000

    added clicker counter element and unit label (step 2 complete)

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,16 @@ document.body.innerHTML = `
   <p>Example image asset: <img src="${exampleIconUrl}" class="icon" /></p>
 `;
 
+let counter: number = 0;
+
 const button = document.createElement("button");
+const counterElement = document.createElement("counter");
 button.textContent = "🐈‍⬛";
 
+button.addEventListener("click", () => {
+  counter += 1;
+  counterElement.textContent = `${counter} cats`;
+});
+
 document.body.appendChild(button);
+document.body.appendChild(counterElement);
