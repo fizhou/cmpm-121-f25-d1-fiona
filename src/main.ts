56f790b5055e245251380077e7f7bcd1b60d303c
Fiona Zhou <fizhou@ucsc.edu>
2025-10-10 22:24:04 +0000

    implemented continuous growth with requestAnimationFrame (step 4 complete)

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,29 @@
 import "./style.css";
 
+let previousTime = performance.now();
 let counter: number = 0;
 
 const button = document.createElement("button");
-const counterElement = document.createElement("counter");
+const counterElement = document.createElement("div");
 button.textContent = "🐈‍⬛";
+counterElement.textContent = "0 cats";
+
+function update(currentTime: number) {
+  const deltaTime = (currentTime - previousTime) / 1000;
+  previousTime = currentTime;
+
+  counter += deltaTime;
+  counterElement.textContent = `${counter.toFixed(1)} cats`;
+  requestAnimationFrame(update);
+}
 
 function handleButtonClick() {
   counter += 1;
-  counterElement.textContent = `${counter} cats`;
+  counterElement.textContent = `${counter.toFixed(1)} cats`;
 }
 
 button.addEventListener("click", handleButtonClick);
 
-setInterval(handleButtonClick, 1000);
-
+requestAnimationFrame(update);
 document.body.appendChild(button);
 document.body.appendChild(counterElement);
