a234240ec1b61e99ba4ec4d820d45162c85c2ac7
Fiona Zhou <fizhou@ucsc.edu>
2025-10-10 22:52:55 +0000

    added purchasable upgrade button (step 5 complete)

--- src/main.ts
+++ src/main.ts
@@ -2,19 +2,43 @@ import "./style.css";
 
 let previousTime = performance.now();
 let counter: number = 0;
+let growthRate = 0;
 
+const UPGRADE_COST = 10;
 const button = document.createElement("button");
+button.id = "button";
+const upgradeButton = document.createElement("button");
+upgradeButton.disabled = true;
+upgradeButton.id = "upgradeButton";
 const counterElement = document.createElement("div");
+
 button.textContent = "🐈‍⬛";
+upgradeButton.textContent = `Upgrade Cat Rate by 1! (-${UPGRADE_COST})`;
 counterElement.textContent = "0 cats";
+
+function updateUpgradeState() {
+  upgradeButton.disabled = counter < UPGRADE_COST;
+}
 
 function update(currentTime: number) {
   const deltaTime = (currentTime - previousTime) / 1000;
   previousTime = currentTime;
 
-  counter += deltaTime;
+  counter += growthRate * deltaTime;
   counterElement.textContent = `${counter.toFixed(1)} cats`;
+
+  updateUpgradeState();
   requestAnimationFrame(update);
+}
+
+function handleUpgradePurchase() {
+  if (counter >= UPGRADE_COST) {
+    counter -= UPGRADE_COST;
+    growthRate += 1;
+    counterElement.textContent = `${counter.toFixed(1)} cats`;
+
+    updateUpgradeState();
+  }
 }
 
 function handleButtonClick() {
@@ -23,7 +47,10 @@ function handleButtonClick() {
 }
 
 button.addEventListener("click", handleButtonClick);
+upgradeButton.addEventListener("click", handleUpgradePurchase);
 
 requestAnimationFrame(update);
+
 document.body.appendChild(button);
+document.body.appendChild(upgradeButton);
 document.body.appendChild(counterElement);
